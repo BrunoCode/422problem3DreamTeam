0803072902c9c3178cdb49c8679e214908cd2d08
Derek <yattha@uw.edu>
2016-04-15 12:01:07 -0700

    al;sdkfj

---
+++ operating_system.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "operating_system.h"
+
+int SYS_STACK = 0;
+
+int main(){
+
+
+
+  return 0;
+}
---
+++ operating_system.h
@@ -0,0 +1,6 @@
+
+
+genProcesses();
+schedulePCBs();
+dispatcher();
+interuptSR();
